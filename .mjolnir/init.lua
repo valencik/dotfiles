79bd069a23b1acc323c1b9becbebc2c3ad5e520f
Andrew Valencik <andrew.valencik@gmail.com>
2014-11-11 20:52:47 -0400

    Modified a fork of Linell's work
    Original work: https://github.com/Linell/mjolnir-config
    Removed dictionary and spotify features.
    I would like this to be more vim like.

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -1,32 +1,46 @@
+-- Load Extensions
 local application = require "mjolnir.application"
-local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
+local hotkey = require "mjolnir.hotkey"
 local fnutils = require "mjolnir.fnutils"
+local alert = require "mjolnir.alert"
+local grid = require "mjolnir.bg.grid"
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "left", function()
-  local win = window.focusedwindow()
-  local f = win:frame()
-  f.x = f.x - 20
-  win:setframe(f)
-end)
+-- Set up hotkey combinations
+local mash = {"cmd", "alt", "ctrl"}
+local mashshift = {"cmd", "alt", "shift"}
+-- Set grid size.
+grid.GRIDWIDTH  = 6
+grid.GRIDHEIGHT = 4
+grid.MARGINX = 0
+grid.MARGINY = 0
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
-  local win = window.focusedwindow()
-  local f = win:frame()
-  f.x = f.x + 20
-  win:setframe(f)
-end)
+hotkey.bind(mash, ';', function() grid.snap(window.focusedwindow()) end)
+hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap) end)
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
-  local win = window.focusedwindow()
-  local f = win:frame()
-  f.y = f.y - 20
-  win:setframe(f)
-end)
+hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
+hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
+hotkey.bind(mashshift, '=', function() grid.adjustheight(1) end)
+hotkey.bind(mashshift, '-', function() grid.adjustheight(-1) end)
 
-hotkey.bind({"cmd", "alt", "ctrl"}, "up", function()
-  local win = window.focusedwindow()
-  local f = win:frame()
-  f.y = f.y + 20
-  win:setframe(f)
-end)
+hotkey.bind(mash, 'left', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mash, 'right', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mash, 'up', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mash, 'down', function() window.focusedwindow():focuswindow_south() end)
+
+hotkey.bind(mash, 'M', grid.maximize_window)
+
+hotkey.bind(mash, 'N', grid.pushwindow_nextscreen)
+hotkey.bind(mash, 'P', grid.pushwindow_prevscreen)
+
+hotkey.bind(mash, 'J', grid.pushwindow_down)
+hotkey.bind(mash, 'K', grid.pushwindow_up)
+hotkey.bind(mash, 'H', grid.pushwindow_left)
+hotkey.bind(mash, 'L', grid.pushwindow_right)
+
+hotkey.bind(mash, 'I', grid.resizewindow_taller)
+hotkey.bind(mash, 'O', grid.resizewindow_wider)
+hotkey.bind(mash, 'Y', grid.resizewindow_thinner)
+hotkey.bind(mash, 'U', grid.resizewindow_shorter)
+
+alert.show("Mjolnir, at your service.")
