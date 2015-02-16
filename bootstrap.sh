941ca335ee0a920c91f8012404bcdf339bddeda2
Andrew Valencik <andrew.valencik@gmail.com>
2015-02-16 00:07:12 -0400

    Added bootstrapping script
    This script install Xcode command line tools
    Installs all brew formulae and casks
    Installs python libraries

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+# Bootstrap a clean OS X install
+
+# Define mesage output types and colours
+ERROR="$(tput setaf 1)ERROR:$(tput sgr 0)"
+BOOTSTRAP="$(tput setaf 2)BOOTSTRAP:$(tput sgr 0)"
+
+echo "${BOOTSTRAP} Installing xcode command line tools - May require user interaction"
+sudo xcode-select --install
+read -p "${BOOTSTRAP} Press any key when Xcode install completes"
+
+echo "${BOOTSTRAP} Installing homebrew..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+echo "${BOOTSTRAP} Installing formulae and casks from .brewfile..."
+brew brewdle
+
+echo "${BOOTSTRAP} Installing python libraries from .pipfile..."
+pip3 install --upgrade -r .pipfile
