faa78a9f6683bf2d02d554c461d5f0bbf7ad9b40
Andrew Valencik <andrew.valencik@gmail.com>
2015-02-16 02:13:59 -0400

    Moved password request upfront.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,12 @@
 ERROR="$(tput setaf 1)ERROR:$(tput sgr 0)"
 BOOTSTRAP="$(tput setaf 2)BOOTSTRAP:$(tput sgr 0)"
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo "${BOOTSTRAP} Installing xcode command line tools - May require user interaction"
 sudo xcode-select --install
 read -p "${BOOTSTRAP} Press any key when Xcode install completes"
