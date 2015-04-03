3a7e0e31e832f9271124404a4dfc2d8aec7a3752
Andrew Valencik <andrew.valencik@gmail.com>
2015-04-03 16:36:14 -0300

    Added symlink setup script

---
+++ symlink-dotfiles.sh
@@ -0,0 +1,7 @@
+#!/bin.bash -e
+
+# Symlink dotfiles to home folder
+ln -s $(pwd)/.zshrc ~/.zshrc
+ln -s $(pwd)/.zshenv ~/.zshenv
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.hammerspoon ~/.hammerspoon
