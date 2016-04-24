dfaae7d0792d41c62cd579f4b93636fc95792589
Andrew Valencik <andrew.valencik@gmail.com>
2016-04-24 20:44:58 -0300

    Made resolution a cli argument for toGif.sh

--- bin/toGif.sh
+++ bin/toGif.sh
@@ -8,7 +8,7 @@ fi
 
 palette="/tmp/palette.png"
 
-filters="fps=15,scale=320:-1:flags=lanczos"
+filters="fps=15,scale=$1:-1:flags=lanczos"
 
-ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
-ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+ffmpeg -v warning -i $2 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $2 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $3
