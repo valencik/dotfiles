806577a533ab6fcb91abff1d1a73f145a3747ee2
Andrew Valencik <andrew.valencik@gmail.com>
2017-09-04 08:40:49 -0400

    Add tmux and zsh theme symlinks

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -6,3 +6,5 @@ ln -s $(pwd)/bin ~/bin
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.zshenv ~/.zshenv
 ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/sunrise-w-job-count.zsh-theme ~/.oh-my-zsh/themes/sunrise-w-job-count.zsh-theme
