820f280db53e471aa0dc1c5ac51960bb48933fb6
Andrew Valencik <andrew.valencik@gmail.com>
2018-01-23 18:57:16 -0500

    Remove orgmode archive from emacs

--- init.el
+++ init.el
@@ -33,7 +33,6 @@
 (require 'package)
 (setq
  package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                    ("org" . "http://orgmode.org/elpa/")
                     ("melpa" . "http://melpa.org/packages/")
                     ("melpa-stable" . "http://stable.melpa.org/packages/"))
  package-archive-priorities '(("melpa-stable" . 1)))
