0cdc5c07b5d92ae5073c35ef67534d8fc355e496
Andrew Valencik <andrew.valencik@gmail.com>
2023-12-27 09:10:51 -0500

    Add groups and ports to media.nix

--- nix/media.nix
+++ nix/media.nix
@@ -3,6 +3,13 @@
 { config, pkgs, ... }:
 
 {
+  users.groups.media.members = [
+    "plex"
+    "sonarr"
+    "radarr"
+    "deluge"
+    "sabnzbd"
+  ];
 
   # Plex
   # https://plex.tv/
@@ -14,29 +21,42 @@
 
   # TV
   # https://sonarr.tv/
+  # http://localhost:8989
   services.sonarr = {
     enable = true;
     openFirewall = true;
+    group = "media";
   };
 
   # Movies
   # https://radarr.video/
+  # http://localhost:7878/
   services.radarr = {
     enable = true;
     openFirewall = true;
+    group = "media";
   };
 
   # Torrents
   # https://deluge-torrent.org/
+  # http://localhost:8112/
   services.deluge = {
     package = pkgs.deluge-2_x;
     enable = true;
     web.enable = true;
+    group = "media";
+    openFirewall = true;
   };
+  # Open Deluge web port in the firewall.
+  networking.firewall.allowedTCPPorts = [
+    8112
+  ];
 
   # Usenet NZB downloader
   # https://sabnzbd.org/
+  # http://localhost:8969/
   services.sabnzbd = {
     enable = true;
+    group = "media";
   };
 }
